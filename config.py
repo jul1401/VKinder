fe887d3ca4e582b74cb05e58cbbb6a90cf642fb5
Юлия Тихонова <115178780+jul1401@users.noreply.github.com>
2023-01-11 08:55:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '0000'
+db_name = 'bot_users'
